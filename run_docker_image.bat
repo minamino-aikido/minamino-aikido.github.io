688dc7c39f9a47fbbf45803d5d996394180ce3c4
Aikige <48190566+aikige@users.noreply.github.com>
2021-04-25 18:29:08 +0900

    [up] run_docker_image.bat: merged docker image build script and docker container execution script. [up] _config.yml: add exclusions, to ignore files under test directory.

--- run_docker_image.bat
+++ run_docker_image.bat
@@ -1,4 +1,14 @@
 set DOCKER_IMAGE=jekyll-env-minamino
 set DOCKER_CONTAINER=jekyll-env-minamino
+docker build -t %DOCKER_IMAGE% .
+
+if "x%~1" == "x" goto run
+goto %~1
+
+:run
+docker run -it --rm -v %CD%:/srv/jekyll --name %DOCKER_CONTAINER% -p 4000:4000 %DOCKER_IMAGE% bundle exec sh ./run.sh
+goto :eof
+
+:bash
 docker run -it --rm -v %CD%:/srv/jekyll --name %DOCKER_CONTAINER% -p 4000:4000 %DOCKER_IMAGE% bash
-rem docker run -it --rm -v %CD%:/srv/jekyll --name %DOCKER_CONTAINER% -p 4000:4000 %DOCKER_IMAGE% bundle exec jekyll serve --host 0.0.0.0 --incremental
+goto :eof
